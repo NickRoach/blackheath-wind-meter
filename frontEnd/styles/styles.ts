b6df18d04f981e5220e6c316903121f1d89659be
NickRoach <njlroach@gmail.com>
2022-12-11 20:04:42 +1100

    set subheading color

--- frontEnd/styles/styles.ts
+++ frontEnd/styles/styles.ts
@@ -9,6 +9,7 @@ export const ButtonContainer = styled.div`
 
 export const Heading = styled.h1`
   text-align: center;
+  color: rgb(55, 55, 71);
   @media (max-width: 699px) {
     font-size: 18px;
   }
@@ -19,6 +20,7 @@ export const Heading = styled.h1`
 
 export const SubHeading = styled.p`
   text-align: center;
+  color: rgb(55, 55, 71);
   @media (max-width: 699px) {
     font-size: 10px;
   }
