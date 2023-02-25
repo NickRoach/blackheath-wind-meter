064cea2320f64dec92067fe0c1fdd6c7d9fcac18
NickRoach <njlroach@gmail.com>
2023-02-25 19:39:05 +1100

    working really nicely, just a bug with double-sending

--- arduino/wind-meter-and-sender/wind-meter-and-sender.ino
+++ arduino/wind-meter-and-sender/wind-meter-and-sender.ino
@@ -3,6 +3,9 @@
 #include <SoftwareSerial.h>
 #include <TinyGsmClient.h>
 #include <LowPower.h>
+#include <ThreeWire.h>  
+#include <RtcDS1302.h>
+
 SoftwareSerial SerialAT(4, 5); // RX, TX
 #if !defined(TINY_GSM_RX_BUFFER)
 #define TINY_GSM_RX_BUFFER 650
@@ -22,16 +25,13 @@ double pulse2 = -1;
 double waitTime = millis();
 boolean timedOut = false;
 int timeToWait = 2000;
-int loopCount = 0;
+int minuteLastSent;
+int dateTimeLastUpdated;
+RtcDateTime rtcTime;
 
-double directionPeriodTimer = millis();
-double sendPeriodTimer = millis();
-double rotationTriggerMoment = millis();
-double rpmPeriodTimer = millis();
 float windDirection;
 float rawDirection;
 float directionAngle;
-bool rpmTriggered = false;
 long rpmSum;
 int rpmMeasurementCount;
 int rpm;
@@ -50,32 +50,54 @@ TinyGsm modem(SerialAT);
 TinyGsmClient client(modem);
 #define SIM_POWER 8
 #define speedPin 2      // black
-#define directionPin A0 // green
+#define directionPin A1 // green
 #define voltagePin A2   // black
                         // VCC: yellow
                         // GND: red (yes, red)
+#define rtcVcc 10
+#define rtcGnd 11
+#define rtcClk 12
+#define rtcDa  13
+#define rtcRst A0
+
+ThreeWire myWire(rtcDa,rtcClk,rtcRst); // IO, SCLK, CE
+RtcDS1302<ThreeWire> Rtc(myWire);
 
 void setup() {
   pinMode(speedPin, INPUT_PULLUP);
   pinMode(directionPin, INPUT_PULLUP);
   pinMode(voltagePin, INPUT);
   pinMode(SIM_POWER, OUTPUT);
+  pinMode(rtcVcc, OUTPUT);
+  pinMode(rtcGnd, OUTPUT);
+  pinMode(rtcClk, OUTPUT);
+  pinMode(rtcDa, OUTPUT);
+  pinMode(rtcRst, OUTPUT);
+
+  digitalWrite(rtcVcc, HIGH);
+  digitalWrite(rtcGnd, LOW);
+
   SerialMon.begin(4800);
   delay(10);
-  SerialMon.println();
   SerialMon.println("Starting...");
+
+  Rtc.Begin();
+  Rtc.SetIsWriteProtected(true);
+  rtcTime = Rtc.GetDateTime();
+  minuteLastSent = rtcTime.Minute();
 }
 
 
 void loop() {
   attachInterrupt(digitalPinToInterrupt(speedPin), recordPulseTime, FALLING);
-  startTime = millis();
   pulse1 = -1;
   pulse2 = -1;
   timedOut = false;
 
-  Serial.print("loopCount: ");
-  Serial.println(loopCount);
+  // this is necessary so that millis() works properly. I don't know why
+  SerialMon.print("");
+  
+  startTime = millis();
 
   // wait for pulse1
   while(timedOut == false && pulse1 == -1) {
@@ -89,18 +111,18 @@ void loop() {
     waitTime = millis();
     if(waitTime >= startTime + timeToWait) timedOut = true;
   }
-
-  // we don't need to listen to the anemometer anymore. Also, interrupts will wake it up from sleep
+  
+  // we don't need to listen to the anemometer anymore. Also, interrupts would wake it up from sleep
   detachInterrupt(digitalPinToInterrupt(speedPin));
 
   calculateAverageRpm();
   checkDirection();
   calculateAverageVoltage();
 
-  ++loopCount;
-  if(loopCount >= 40){
+  rtcTime = Rtc.GetDateTime();
+  if(rtcTime.Minute() % 5 == 0 && rtcTime.Minute() != minuteLastSent){
     sendData();
-    loopCount = 0;
+    minuteLastSent = rtcTime.Minute();
     voltageSampleCount = 0;
     resetMinMaxAv();
   }
@@ -114,101 +136,89 @@ void loop() {
 
 void recordPulseTime(){
   if(pulse1 == -1){
-//    SerialMon.println("recording pulse 1");
       pulse1 = millis();
     }
+  // 20ms time to alleviate switch noise. This limits the maximum measurable wind speed to about 100kt
   else if (pulse2 == -1 && millis() - pulse1 > 20){
-//    SerialMon.println("recording pulse 2");
     pulse2 = millis();
   }
 }
 
 void calculateAverageRpm() {
 // if anemometer isn't spinning, set rpm as 0
-    if(timedOut == true) {
-        rpm = 0;
-      }
-      else {
-        rpm = 1/(((pulse2 - pulse1)/1000)/60);
-      }
-
-    if(rpm > rpmMax) rpmMax = rpm;
-    if((rpm < rpmMin) || rpmMin == -1) rpmMin = rpm;
-    
-    rpmSum += rpm;
-    rpmMeasurementCount++;
-    
-    rpmAv = rpmSum / rpmMeasurementCount;
-    rpmTriggered = false;
-}
-
-void checkDirection(){
-  // if the air isn't moving, direction is meaningless
-  if(rpm == 0) return;
-  rawDirection = analogRead(directionPin);
-  sector = rawDirection + 32;
-  if(sector > 1024){
-    sector = sector - 1024;
+  if(timedOut == true) {
+      rpm = 0;
+//      SerialMon.println("timed out");
     }
-  sectorNumber = int(sector / 64);
-  sectorCounter[sectorNumber]++;
-}
-
-void calculateAverageVoltage() {
-  voltageSample = analogRead(voltagePin);
-  voltageSample = voltageSample / 1024 * 5; 
-  voltage = ((voltage * voltageSampleCount) + voltageSample) / ++voltageSampleCount;
-}
-
-void resetMinMaxAv(){
-  rpmMin = -1;
-  rpmMax = 0;
-  rpmSum = 0;
-  rpmMeasurementCount = 0;
-  for(int i = 0; i <= 15; i++){
-    sectorCounter[i] = 0;
-  }
+    else {
+      rpm = 1/(((pulse2 - pulse1)/1000)/60);
+//      SerialMon.println("rpm set");
+    }
+  if(rpm > rpmMax) rpmMax = rpm;
+  if((rpm < rpmMin) || rpmMin == -1) rpmMin = rpm;
+  rpmSum += rpm;
+  rpmMeasurementCount++;
+  rpmAv = rpmSum / rpmMeasurementCount;
 }
 
 void sendData(){
-  if(voltage < 3.6) {
-    SerialMon.println("low voltage");
-    return;
-  }
+  //  if(voltage < 3.6) return;
+  SerialMon.println("Sending data...");
   digitalWrite(SIM_POWER, HIGH);
   delay(100);
-  SerialMon.println("Sending data...");
   SerialAT.begin(57600);
   modem.restart();
+
+  // just after midnight each day, update the RTC with network time
+  if(rtcTime.Day() != dateTimeLastUpdated){
+  int   year     = 0;
+  int   month    = 0;
+  int   day      = 0;
+  int   hour     = 0;
+  int   minute   = 0;
+  int   second   = 0;
+  float timezone = 0;
+  if (modem.getNetworkTime(&year, &month, &day, &hour, &minute, &second, &timezone)) {
+    RtcDateTime gsmTime = RtcDateTime(year, month, day, hour, minute, second);
+    SerialMon.println("Setting time with network time");
+    Rtc.SetIsWriteProtected(false);
+    Rtc.SetDateTime(gsmTime);
+    Rtc.SetIsWriteProtected(true);
+    dateTimeLastUpdated = gsmTime.Day();
+    } else {
+      SerialMon.println("Couldn't get network time, retrying next send cycle");
+    }
+  }
+  
   SerialMon.print("Waiting for network...");
   if (!modem.waitForNetwork()) {
      SerialMon.println(" fail");
     delay(1000);
     return;
   }
-   SerialMon.println(" success");
+  SerialMon.println(" success");
   if (modem.isNetworkConnected()) {
      SerialMon.println("Network connected");
   }
 
-   SerialMon.print(F("Connecting to "));
-   SerialMon.print(apn);
+  SerialMon.print(F("Connecting to "));
+  SerialMon.print(apn);
   if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-     SerialMon.println(" fail");
+    SerialMon.println(" fail");
     delay(1000);
     return;
   }
-   SerialMon.println(" success");
+  SerialMon.println(" success");
 
   if (modem.isGprsConnected()) {
-     SerialMon.println("GPRS connected");
+    SerialMon.println("GPRS connected");
   }
 
   if (!client.connect(server, port)) {
-     SerialMon.println(" fail");
+    SerialMon.println(" fail");
   }
 
-  SerialMon.println("Performing HTTP POST request...");
+  SerialMon.println("Performing HTTP POST...");
   String httpRequestData = getJsonString();
   client.print(String("POST ") + resource + " HTTP/1.1\r\n");
   client.print(String("Host: ") + server + "\r\n");
@@ -226,6 +236,42 @@ void sendData(){
   SerialMon.println();
   digitalWrite(SIM_POWER, LOW);
 }
+
+
+
+void checkDirection(){
+  // if the air isn't moving, direction is meaningless
+  if(rpm == 0) return;
+  rawDirection = analogRead(directionPin);
+  sector = rawDirection + 32;
+  if(sector > 1024){
+    sector = sector - 1024;
+    }
+  sectorNumber = int(sector / 64);
+  sectorCounter[sectorNumber]++;
+}
+
+
+
+void calculateAverageVoltage() {
+  voltageSample = analogRead(voltagePin);
+  voltageSample = voltageSample / 1024 * 5; 
+  voltage = ((voltage * voltageSampleCount) + voltageSample) / ++voltageSampleCount;
+}
+
+
+
+void resetMinMaxAv(){
+  rpmMin = -1;
+  rpmMax = 0;
+  rpmSum = 0;
+  rpmMeasurementCount = 0;
+  for(int i = 0; i <= 15; i++){
+    sectorCounter[i] = 0;
+  }
+}
+
+
 
 String getJsonString(){
   String jsonString;
