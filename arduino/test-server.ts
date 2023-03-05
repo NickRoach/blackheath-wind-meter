8eec519f1604f102d292aee2515a2a4035e6b2aa
NickRoach <njlroach@gmail.com>
2023-03-05 20:21:34 +1100

    have test server console log the headers

--- arduino/test-server.ts
+++ arduino/test-server.ts
@@ -1,15 +1,16 @@
-import express from 'express';
-import bodyParser from 'body-parser'
+import express from "express";
+import bodyParser from "body-parser";
 const app = express();
 const port = 8080;
 
 const jsonParser = bodyParser.json();
 
-app.post('/', jsonParser, (req, res) => {
-  res.send('got it');
-  console.log('req.body: ', req.body);
-})
+app.post("/blackheath", jsonParser, (req, res) => {
+  res.send("got it");
+  console.log("req.body: ", req.body);
+  console.log("req.headers: ", req.headers);
+});
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
