0861f5567817a616c66e0a38dc2a33155d5caea9
NickRoach <njlroach@gmail.com>
2023-03-14 07:35:14 +1100

    modify hotjar script application, add google analytics

--- frontEnd/pages/_app.tsx
+++ frontEnd/pages/_app.tsx
@@ -1,15 +1,39 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { hotjar } from "react-hotjar";
-import { useEffect } from "react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    hotjar.initialize(
-      Number(process.env.NEXT_PUBLIC_HJID),
-      Number(process.env.NEXT_PUBLIC_HJSV)
-    );
-  }, []);
+  return (
+    <>
+      <Script id="hotjar">
+        {`
+          (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3405685,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
+      </Script>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`}
+      />
 
-  return <Component {...pageProps} />;
+      <Script id="googleTag" strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'XXXXXXXXXX', {
+          page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
