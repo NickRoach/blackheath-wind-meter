f9f2445241e112d5cb116d9e656efc3ffb45edd2
NickRoach <njlroach@gmail.com>
2023-03-14 07:04:19 +1100

    feat: initialize hotjar

--- frontEnd/pages/_app.tsx
+++ frontEnd/pages/_app.tsx
@@ -1,6 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { hotjar } from "react-hotjar";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    hotjar.initialize(
+      Number(process.env.NEXT_PUBLIC_HJID),
+      Number(process.env.NEXT_PUBLIC_HJSV)
+    );
+  }, []);
+
   return <Component {...pageProps} />;
 }
