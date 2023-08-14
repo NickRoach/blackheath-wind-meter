d58bdd8f7577e1d45b1e0b3f1dd1de3a76eee0ee
NickRoach <njlroach@gmail.com>
2023-08-14 18:42:00 +1000

    wip

--- arduino/wind-meter-and-sender/wind-meter-and-sender.ino
+++ arduino/wind-meter-and-sender/wind-meter-and-sender.ino
@@ -28,21 +28,12 @@ int timeToWait = 2000;
 int minuteLastSent = -1;
 int dateTimeLastUpdated = -1;
 RtcDateTime rtcTime;
-float windDirection;
-float rawDirection;
-float directionAngle;
-long rpmSum;
-int rpmMeasurementCount;
-int rpm;
-int rpmMin;
-int rpmMax;
-int rpmAv;
-float sector;
-int sectorNumber;
-int sectorCounter[16];
+int rawDirection;
+int pulseDelay;
 float voltage;
 float voltageSample;
 int voltageSampleCount = 0;
+String jsonString;
 
 TinyGsm modem(SerialAT);
 TinyGsmClient client(modem);
@@ -117,20 +108,27 @@ void loop() {
   // we don't need to listen to the anemometer anymore. Also, interrupts would wake it up from sleep
   detachInterrupt(digitalPinToInterrupt(speedPin));
 
-  calculateAverageRpm();
   checkDirection();
   calculateAverageVoltage();
+  pulseDelay = (pulse2 - pulse1)/2;
+  SerialMon.print("pulse delay: ");
+  if(!timedOut)  SerialMon.println(pulseDelay);
+
+  if(timedOut == true) {
+    jsonString += "0,0,";
+  } else {
+  }
   
   rtcTime = Rtc.GetDateTime();
     
   if(rtcTime.Minute() % 15 == 0 && rtcTime.Minute() != minuteLastSent){
     minuteLastSent = rtcTime.Minute();
-    sendData();
+//    sendData();
     resetVariables();
   }
   
   Serial.flush();
-  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
+//  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
 }
 
 
@@ -147,118 +145,94 @@ void recordPulseTime(){
 }
 
 
-
-void calculateAverageRpm(){
-// if anemometer isn't spinning, set rpm as 0
-  if(timedOut == true) {
-      rpm = 0;
-    // SerialMon.println("timed out");
-    }
-    else {
-      rpm = 1/(((pulse2 - pulse1)/1000)/60);
-    // SerialMon.println("rpm set");
-    }
-  if(rpm > rpmMax) rpmMax = rpm;
-  if((rpm < rpmMin) || rpmMin == -1) rpmMin = rpm;
-  rpmSum += rpm;
-  rpmMeasurementCount++;
-  rpmAv = rpmSum / rpmMeasurementCount;
-}
-
-
-
-void sendData(){
-  if(voltage < 3.7) return;
-  SerialMon.println("Sending data...");
-  digitalWrite(SIM_POWER, HIGH);
-  delay(100);
-  SerialAT.begin(57600);
-  modem.restart();
-
-  SerialMon.print("Waiting for network...");
-  if (!modem.waitForNetwork()) {
-     SerialMon.println(" fail");
-    delay(1000);
-    return;
-  }
-  SerialMon.println(" success");
-  if (modem.isNetworkConnected()) {
-     SerialMon.println("Network connected");
-  }
-
-  // just after midnight each day, update the RTC with network time
-  if(rtcTime.Day() != dateTimeLastUpdated || rtcTime.Year() == 2080){
-  SerialMon.print("Setting time with network time... ");
-  int   year     = 0;
-  int   month    = 0;
-  int   day      = 0;
-  int   hour     = 0;
-  int   minute   = 0;
-  int   second   = 0;
-  float timezone = 0;
-  if (modem.getNetworkTime(&year, &month, &day, &hour, &minute, &second, &timezone)) {
-    if(year != 2080){
-      RtcDateTime gsmTime = RtcDateTime(year, month, day, hour, minute, second);
-      Rtc.SetIsWriteProtected(false);
-      Rtc.SetDateTime(gsmTime);
-      Rtc.SetIsWriteProtected(true);
-      dateTimeLastUpdated = gsmTime.Day();
-      SerialMon.println("done");
-      }
-    } else {
-      SerialMon.println("couldn't get network time, retrying next send cycle");
-    }
-  }
-  
-  SerialMon.print(F("Connecting to "));
-  SerialMon.print(apn);
-  SerialMon.print("...");
-  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-    SerialMon.println(" fail");
-    delay(1000);
-    return;
-  }
-  SerialMon.println(" success");
-
-  if (modem.isGprsConnected()) {
-    SerialMon.println("GPRS connected");
-  }
-
-  if (!client.connect(server, port)) {
-    SerialMon.println(" fail");
-  }
-
-  SerialMon.println("Performing HTTP POST...");
-  String httpRequestData = getJsonString(rtcTime);
-  client.print(String("POST ") + resource + " HTTP/1.1\r\n");
-  client.print(String("Host: ") + server + "\r\n");
-  client.println("Connection: close");
-  client.println("Content-Type: application/json");
-  client.print("password: ");
-  client.println(password);
-  client.print("Content-Length: ");
-  client.println(httpRequestData.length());
-  client.println();
-  client.println(httpRequestData);
-  client.stop();
-  SerialMon.println("Done");
-  digitalWrite(SIM_POWER, LOW);
-  SerialMon.println();
-  SerialMon.println("Recording wind data...");
-}
+//
+//void sendData(){
+//  if(voltage < 3.7) return;
+//  SerialMon.println("Sending data...");
+//  digitalWrite(SIM_POWER, HIGH);
+//  delay(100);
+//  SerialAT.begin(57600);
+//  modem.restart();
+//
+//  SerialMon.print("Waiting for network...");
+//  if (!modem.waitForNetwork()) {
+//     SerialMon.println(" fail");
+//    delay(1000);
+//    return;
+//  }
+//  SerialMon.println(" success");
+//  if (modem.isNetworkConnected()) {
+//     SerialMon.println("Network connected");
+//  }
+//
+//  // at 3am each day, update the RTC with network time
+//  if((rtcTime.Day() != dateTimeLastUpdated && rtcTime.Hour() == 3) || rtcTime.Year() == 2080){
+//  SerialMon.print("Setting time with network time... ");
+//  int   year     = 0;
+//  int   month    = 0;
+//  int   day      = 0;
+//  int   hour     = 0;
+//  int   minute   = 0;
+//  int   second   = 0;
+//  float timezone = 0;
+//  if (modem.getNetworkTime(&year, &month, &day, &hour, &minute, &second, &timezone)) {
+//    if(year != 2080){
+//      RtcDateTime gsmTime = RtcDateTime(year, month, day, hour, minute, second);
+//      Rtc.SetIsWriteProtected(false);
+//      Rtc.SetDateTime(gsmTime);
+//      Rtc.SetIsWriteProtected(true);
+//      dateTimeLastUpdated = gsmTime.Day();
+//      SerialMon.println("done");
+//      }
+//    } else {
+//      SerialMon.println("couldn't get network time, retrying next send cycle");
+//    }
+//  }
+//  
+//  SerialMon.print(F("Connecting to "));
+//  SerialMon.print(apn);
+//  SerialMon.print("...");
+//  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+//    SerialMon.println(" fail");
+//    delay(1000);
+//    return;
+//  }
+//  SerialMon.println(" success");
+//
+//  if (modem.isGprsConnected()) {
+//    SerialMon.println("GPRS connected");
+//  }
+//
+//  if (!client.connect(server, port)) {
+//    SerialMon.println(" fail");
+//  }
+//
+//  SerialMon.println("Performing HTTP POST...");
+//  String httpRequestData = getJsonString(rtcTime);
+//  client.print(String("POST ") + resource + " HTTP/1.1\r\n");
+//  client.print(String("Host: ") + server + "\r\n");
+//  client.println("Connection: close");
+//  client.println("Content-Type: application/json");
+//  client.print("password: ");
+//  client.println(password);
+//  client.print("Content-Length: ");
+//  client.println(httpRequestData.length());
+//  client.println();
+//  client.println(httpRequestData);
+//  client.stop();
+//  SerialMon.println("Done");
+//  digitalWrite(SIM_POWER, LOW);
+//  SerialMon.println();
+//  SerialMon.println("Recording wind data...");
+//}
 
 
 
 void checkDirection(){
   // if the air isn't moving, direction is meaningless
-  if(rpm == 0) return;
   rawDirection = analogRead(directionPin);
-  sector = rawDirection + 32;
-  if(sector > 1024){
-    sector = sector - 1024;
-    }
-  sectorNumber = int(sector / 64);
-  sectorCounter[sectorNumber]++;
+  SerialMon.print("rawDirection: ");
+  SerialMon.println(rawDirection/2);
 }
 
 
@@ -275,52 +249,6 @@ void calculateAverageVoltage() {
 
 
 void resetVariables(){
-  rpmMin = -1;
-  rpmMax = 0;
-  rpmSum = 0;
-  rpmMeasurementCount = 0;
-  for(int i = 0; i <= 15; i++){
-    sectorCounter[i] = 0;
-  }
+  jsonString = "[";
   voltageSampleCount = 0;
-}
-
-
-
-String getJsonString(RtcDateTime sendTime){
-  String jsonString;
-  
-  jsonString += "{\"RPMMax\":";
-  jsonString += rpmMax;
-  jsonString += ",\"RPMMin\":";
-  jsonString += rpmMin;
-  jsonString += ",\"RPMAverage\":";
-  jsonString += rpmAv;
-  jsonString += ",\"sectorData\":";
-  jsonString += "[";
-  for(int i = 0; i <= 15; i++){
-    jsonString += sectorCounter[i];
-    if(i < 15){
-        jsonString += ",";
-      }
-  };
-  jsonString += "]";
-  jsonString += ",\"time\":";
-  jsonString += "\"";
-  jsonString += sendTime.Year();
-  jsonString += "/";
-  jsonString += sendTime.Month();
-  jsonString += "/";
-  jsonString += sendTime.Day();
-  jsonString += " ";
-  if(sendTime.Hour() < 10) jsonString += "0";
-  jsonString += sendTime.Hour();
-  jsonString += ":";
-  if(sendTime.Minute() < 10) jsonString += "0";
-  jsonString += sendTime.Minute();
-  jsonString += "\"";
-  jsonString += ",\"voltage\":";
-  jsonString += voltage;
-  jsonString += "}";
-  return jsonString;
 }
