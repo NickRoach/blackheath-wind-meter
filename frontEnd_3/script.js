9b1b1a2cce6d31bcb509cc50a28be33f3a8c93bc
NickRoach <njlroach@gmail.com>
2023-08-22 10:44:07 +1000

    prettier loading

--- frontEnd_3/script.js
+++ frontEnd_3/script.js
@@ -221,6 +221,7 @@ const drawDataPoints = () => {
 
 const drawChart = (statsBox) => {
   // create the chart canvas at the right size
+  document.getElementById('loading').remove()
   const chart = document.createElement('canvas')
   chart.id = 'chart'
   chart.height = 350
